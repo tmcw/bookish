0a6a521604556bb0fa7b9053fb0122676b5173c8
Tom MacWright <tom@macwright.org>
2019-02-20 21:17:53 -0800

    alias

--- pages/search.js
+++ pages/search.js
@@ -32,10 +32,28 @@ export default class Search extends React.Component {
         </head>
         <div className="mw6 ph4-ns sans-serif center">
           <div className="flex justify-between items-end">
-            <h1 className="f3 mt4 mb0">Booky</h1>
+            <h1 className="f3 mt4 mb0">Bookish</h1>
             <a href="/help">help</a>
           </div>
-
+          <form className="flex pv3" action="/search">
+            <input
+              name="id"
+              type="text"
+              className="f5 pa2 code flex-auto mr1 ba b--black-50 br2"
+            />
+            <select name="type" className="f5 ba b--black-50 mr1">
+              <option value="isbn">ISBN</option>
+              <option value="olid">OLID</option>
+              <option value="oclc">OCLC</option>
+              <option value="lccn">LCCN</option>
+              <option value="goodreads">GoodReads</option>
+            </select>
+            <input
+              className="ba b--black-50 bg-light-yellow br2"
+              type="submit"
+              value="Search"
+            />
+          </form>
           <div className="pv3">
             <div className="br2 pa2 bg-light-yellow flex justify-between">
               <span>Input: {id}</span>
